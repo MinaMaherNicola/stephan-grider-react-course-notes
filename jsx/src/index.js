e8420af0a7696b24df5d73e353fd214a3da7cf42
MinaMaherNicola <minamahernicola@outlook.com>
2021-01-16 09:38:04 +0200

    Finished section 2: Build content with JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,14 +2,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+  return 'Click Me';
+}
+
 // Create a react component
 const App = () => {
-  return <div>Hi There!</div>;
+  const buttonText = 'Click Me!';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText}
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector('#root'));
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
+if (module.hot) {
+  module.hot.accept();
+}
