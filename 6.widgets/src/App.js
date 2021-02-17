2c0eaddc0da6489e461903ebd236b479b02aa925
MinaMaherNicola <minamahernicola@outlook.com>
2021-02-17 18:03:11 +0200

    Finished section 13: Navigation from Scratch

--- 6.widgets/src/App.js
+++ 6.widgets/src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
   {
@@ -37,8 +39,28 @@ const options = [
 const App = () => {
   const [selected, setSelected] = useState(options[0]);
   return (
-    <div className="ui container">
-      <Translate />
+    <div className="ui container" style={{ marginTop: '20px' }}>
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+
+      <Route path="/search">
+        <Search />
+      </Route>
+
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a Color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
