ea58b1fec3ec203c985e6e9a5a2e46d821a740ab
Mina Maher <minamahernicola@outlook.com>
2021-04-16 19:02:20 +0200

    Finished section 23 Redux-Forms

--- 9.streams/client/src/reducers/index.js
+++ 9.streams/client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import authRedcuer from './authReducer';
 
 export default combineReducers({
-  auth: authRedcuer
+  auth: authRedcuer,
+  form: formReducer
 });
